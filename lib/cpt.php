979c567241bdab4584d0ec12d1fe521f4454a2b5
Tristan MacInnis <mail@tristanmacinnis.com>
2015-12-30 16:21:50 -0700

    alignment

--- lib/cpt.php
+++ lib/cpt.php
@@ -14,25 +14,29 @@ add_action( 'after_switch_theme', __NAMESPACE__ . '\\flush_rewrite_rules' );
  * The custom type
  */
 function custom_post_example() {
-  // http://codex.wordpress.org/Function_Reference/register_post_type
+  /* http://codex.wordpress.org/Function_Reference/register_post_type */
 	register_post_type( 'custom_type',
-		array( 'labels' => array(
-			'name' => __( 'Custom Types', 'sage' ),
-			'singular_name' => __( 'Custom Post', 'sage' ),
+		array( 'labels'         => array(
+			'name'                => __( 'Custom Types', 'sage' ),
+			'singular_name'       => __( 'Custom Post', 'sage' ),
 			),
-			'description' => __( 'This is the example custom post type', 'sage' ),
-			'public' => true,
-			'publicly_queryable' => true,
+			'description'         => __( 'This is the example custom post type', 'sage' ),
+			'public'              => true,
+			'publicly_queryable'  => true,
 			'exclude_from_search' => false,
-			'show_ui' => true,
-			'query_var' => true,
-			'menu_position' => 8,
-			'menu_icon' => 'dashicons-book', /* https://developer.wordpress.org/resource/dashicons/#book */
-			'rewrite'	=> array( 'slug' => 'custom_type', 'with_front' => false ), /* you can specify its url slug */
-			'has_archive' => 'custom_type', /* you can rename the slug here */
-			'capability_type' => 'post',
-			'hierarchical' => false,
-			'supports' => array( 'title', 'editor', 'thumbnail', 'page-attributes')
+			'show_ui'             => true,
+			'query_var'           => true,
+			'menu_position'       => 8,
+      /* https://developer.wordpress.org/resource/dashicons */
+			'menu_icon'           => 'dashicons-book',
+			'rewrite'	            => array(
+        'slug'              => 'custom_type',
+        'with_front'        => false
+      ),
+			'has_archive'         => 'custom_type',
+			'capability_type'     => 'post',
+			'hierarchical'        => false,
+			'supports'            => array( 'title', 'editor', 'thumbnail', 'page-attributes')
 		)
 	);
 }
