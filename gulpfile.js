98b6472e090bde158f667403f178b3fb50c37ad7
Ben Bateman <ben@murmurcreative.com>
2016-01-29 13:59:01 -0800

    switch from css-minify to cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var jshint       = require('gulp-jshint');
 var lazypipe     = require('lazypipe');
 var less         = require('gulp-less');
 var merge        = require('merge-stream');
-var minifyCss    = require('gulp-minify-css');
+var cssnano     = require('gulp-cssnano');
 var plumber      = require('gulp-plumber');
 var rev          = require('gulp-rev');
 var runSequence  = require('run-sequence');
@@ -105,10 +105,7 @@ var cssTasks = function(filename) {
         'opera 12'
       ]
     })
-    .pipe(minifyCss, {
-      advanced: false,
-      rebase: false
-    })
+    .pipe(cssnano)
     .pipe(function() {
       return gulpif(enabled.rev, rev());
     })
