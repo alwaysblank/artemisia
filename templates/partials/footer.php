b5a6b345287d2cdbbe4a5407bd989836fc866e32
Tristan MacInnis <tristan@macinnis.net>
2016-09-26 14:40:25 -0600

    changing footer class and adding default murmur link-back snippet.

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,5 +1,6 @@
-<footer class="content-info">
+<footer class="colophon">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <a href="http://murmurcreative.com" <?php if (!is_front_page()) {echo 'rel="nofollow"';} ?> target="_blank">murmur</a>
   </div>
 </footer>
